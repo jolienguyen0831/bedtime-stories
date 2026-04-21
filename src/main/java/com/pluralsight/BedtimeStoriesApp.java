e117a0304c013ab9134f30a72ddd8c20a8bb8404
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-04-21 10:21:33 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/BedtimeStoriesApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class BedtimeStories {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        FileReader fileReader = new 
+
+    }
+}
