72cd015e5c3ed0ddc39925610d1ab6e209c1bee9
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-04-21 10:26:27 -0500

    read the files from user input

--- src/main/java/com/pluralsight/BedtimeStoriesApp.java
+++ src/main/java/com/pluralsight/BedtimeStoriesApp.java
@@ -1,13 +1,30 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class BedtimeStories {
-    private static Scanner scanner = new Scanner(System.in);
+public class BedtimeStoriesApp {
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.print("Please enter the filename what you want to read: ");
+        String userInputFileName = input.nextLine();
 
-        FileReader fileReader = new 
+        try {
+            FileReader fileReader = new FileReader(userInputFileName);
 
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+
+            while(line!= null){
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("The file could not be read. Please make sure the file is available and not locked and then try again.");
+            e.printStackTrace();
+        }
     }
 }
